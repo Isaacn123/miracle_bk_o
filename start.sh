9d85bc2e23fcad71562d8938f4523be438901453
Isaacn123 <nsambai72@gmail.com>
2024-06-20 20:09:26 +0300

    add sart Script for Gurncorn

--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-exec gunicorn -c /home/yeshambn/mcc.backup.miracle44radio.com/gunicorn.conf.py main:app
+source /home/yeshambn/virtualenv/mcc.backup.miracle44radio.com/3.9/bin/activate 
+# exec gunicorn -c /home/yeshambn/mcc.backup.miracle44radio.com/gunicorn.conf.py main:app
+exec gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
+
